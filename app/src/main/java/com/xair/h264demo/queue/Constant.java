97ab6f7e94c9bb2508b280f81ac22be92ed6f2a8
pj <734552377@qq.com>
2019-05-06 14:04:44 +0800

    比较稳定的版本2

--- app/src/main/java/com/xair/h264demo/queue/Constant.java
+++ app/src/main/java/com/xair/h264demo/queue/Constant.java
@@ -8,9 +8,9 @@ public class Constant {
 
     public static final String MIME_TYPE = "video/avc";
 
-    public static final int VIDEO_WIDTH = 480 ;
+    public static final int VIDEO_WIDTH = 640 ;
 
-    public static final int VIDEO_HEIGHT = 320;
+    public static final int VIDEO_HEIGHT = 360;
 
     public static final int VIDEO_DPI = 1;
 
